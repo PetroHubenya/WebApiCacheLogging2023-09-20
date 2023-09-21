ffda20a70b6db2c8d5eb3a46ed04a1dcf453f340
Petro Hubenya <p.hubenya@gmail.com>
2023-09-20 22:01:24 -0400

    MongoDBService is done. No methods yet.

---
+++ DataAccessLayer/MongoDBService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using Models;
+using MongoDB.Driver;
+
+namespace DataAccessLayer
+{
+    public class MongoDBService
+    {
+        private readonly IMongoCollection<Box> _boxCollection;
+
+        private readonly IMongoCollection<Sensor> _sensorCollection;
+
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings)
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURL);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _boxCollection = database.GetCollection<Box>(mongoDBSettings.Value.CollectionNameBoxes);
+            _sensorCollection = database.GetCollection<Sensor>(mongoDBSettings.Value.CollectionNameSensors);
+        }
+
+
+
+    }
+}
--- Models/MongoDBSettings.cs
+++ Models/MongoDBSettings.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class MongoDBSettings
+    public class MongoDBSettings
     {
         public string ConnectionURL { get; set; } = null!;
 
