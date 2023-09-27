65968753f1984f9e4f53b405b24856479db89688
Petro Hubenya <p.hubenya@gmail.com>
2023-09-26 21:13:49 -0400

    SensorDataCotroller methods are done.

--- WebApi/Controllers/SensorDataController.cs
+++ WebApi/Controllers/SensorDataController.cs
@@ -1,6 +1,8 @@
 ﻿using Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Models;
+using MongoDB.Driver;
 
 namespace WebApi.Controllers
 {
@@ -13,6 +15,40 @@ namespace WebApi.Controllers
         public SensorDataController(ISensorDataService sensorDataService)
         {
             _sensorDataService = sensorDataService;
+        }
+
+        // Create SensorData for the specific sensor.
+        public async Task<IActionResult> CreateSensorDataAsync(SensorData sensorData)
+        {
+            await _sensorDataService.CreateSensorDataAsync(sensorData);
+
+            return Ok("SensorData is stored into the database.");
+        }
+
+        // Get SensorData of the specific sensor.
+        public async Task<ActionResult<List<SensorData>>> GetSensorsDataBySensorIdAsync(string sensorId)
+        {
+            var result = await _sensorDataService.GetSensorsDataBySensorIdAsync(sensorId);
+
+            return Ok(result);
+        }
+
+        // Get SensorData of the specific sensor with pagination.
+        public async Task<ActionResult<(int totalPages, IReadOnlyList<SensorData> readOnlyList)>> GetSensorsDataPaginationAsync(string sensorId,
+                                                                                                                  int page,
+                                                                                                                  int pageSize)
+        {
+            var result = await _sensorDataService.GetSensorsDataPaginationAsync(sensorId, page, pageSize);
+            
+            return Ok(result);
+        }
+
+        // Delete all SensorData of the specific sensor.
+        public async Task<ActionResult<DeleteResult>> DeleteAllSensorDataBySensorIdAsync(string sensorId)
+        {
+            var result = await _sensorDataService.DeleteAllSensorDataBySensorIdAsync(sensorId);
+
+            return Ok(result);
         }
     }
 }
