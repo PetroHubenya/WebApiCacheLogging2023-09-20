d2b6e08a6fbe4d1291295e52906bf286be641c48
Petro Hubenya <p.hubenya@gmail.com>
2023-09-27 22:00:04 -0400

    BoxService methods are updated with Exceptions.

--- BusinessLogicLayer/BoxService.cs
+++ BusinessLogicLayer/BoxService.cs
@@ -16,48 +16,158 @@ namespace BusinessLogicLayer
         // Create box
         public async Task CreateBoxAsync(Box box)
         {
-            await _dataService.CreateBoxAsync(box);
+            try
+            {
+                if (box == null)
+                {
+                    throw new ArgumentNullException(nameof(box));
+                }
+                else
+                {
+                    await _dataService.CreateBoxAsync(box);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         // Get all boxes
         public async Task<List<Box>> GetAllBoxesAsync()
         {
-            return await _dataService.GetAllBoxesAsync();
+            try
+            {
+                var result = await _dataService.GetAllBoxesAsync();
+
+                if (result == null)
+                {
+                    throw new Exception("The list of boxes is not found.");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         // Get box by id.
         public async Task<Box> GetBoxAsync(string id)
         {
-            return await _dataService.GetBoxAsync(id);
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentNullException(nameof(id));
+                }
+                else
+                {
+                    var result = await _dataService.GetBoxAsync(id);
+
+                    if (result == null)
+                    {
+                        throw new Exception($"The box with the {id} Id is not found.");
+                    }
+                    else
+                    {
+                        return result;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         // Update box.
         public async Task UpdateBoxAsync(string id, Box newBox)
         {
-            await _dataService.UpdateBoxAsync(id, newBox);
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentNullException(nameof(id));
+                }
+                else if (newBox == null)
+                {
+                    throw new ArgumentNullException(nameof(newBox));
+                }
+                else
+                {
+                    await _dataService.UpdateBoxAsync(id, newBox);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         // Delete box.
         public async Task DeleteBoxAsync(string id)
         {
-            // Delete all the SensorData for the correspondent sensors.
-
-            var box = await _dataService.GetBoxAsync(id);
-
-            foreach (string sensorId in box.SensorIds)
+            try
             {
-                await _dataService.DeleteAllSensorDataBySensorIdAsync(sensorId);
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentNullException(nameof(id));
+                }
+                else
+                {
+                    // Delete all the SensorData for the correspondent sensors.
+
+                    var box = await _dataService.GetBoxAsync(id);
+
+                    if (box == null)
+                    {
+                        throw new Exception($"The box with the {id} Id is not found.");
+                    }
+                    else
+                    {
+                        foreach (string sensorId in box.SensorIds)
+                        {
+                            await _dataService.DeleteAllSensorDataBySensorIdAsync(sensorId);
+                        }
+
+                        // Delete the box.
+
+                        await _dataService.DeleteBoxAsync(id);
+                    }
+                }
             }
-
-            // Delete the box.
-
-            await _dataService.DeleteBoxAsync(id);
+            catch
+            {
+                throw;
+            }
         }
 
         // Add sensor to the box.
         public async Task AddSensorToBoxAsync(string boxId, string sensorId)
         {
-            await _dataService.AddSensorToBoxAsync(boxId, sensorId);
+            try
+            {
+                if (string.IsNullOrEmpty(boxId))
+                {
+                    throw new ArgumentNullException(nameof(boxId));
+                }
+                else if (sensorId == null)
+                {
+                    throw new ArgumentNullException(nameof(sensorId));
+                }
+                else
+                {
+                    await _dataService.AddSensorToBoxAsync(boxId, sensorId);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
