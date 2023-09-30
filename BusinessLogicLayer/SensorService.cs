990bd7f7e514a147a2a1c400f4f1dac5fff17e1a
Petro Hubenya <p.hubenya@gmail.com>
2023-09-29 22:07:23 -0400

    SensorService methods updated with Exceptions.

--- BusinessLogicLayer/SensorService.cs
+++ BusinessLogicLayer/SensorService.cs
@@ -21,33 +21,118 @@ namespace BusinessLogicLayer
         // Create sensor.
         public async Task CreateSensorAsync(Sensor sensor)
         {
-            await _dataService.CreateSensorAsync(sensor);
+            try
+            {
+                if(sensor == null)
+                {
+                    throw new ArgumentNullException(nameof(sensor));
+                }
+                else
+                {
+                    await _dataService.CreateSensorAsync(sensor);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         // Get all sensors.
         public async Task<List<Sensor>> GetAllSensorsAsync()
         {
-            return await _dataService.GetAllSensorsAsync();
+            try
+            {
+                var result = await _dataService.GetAllSensorsAsync();
+
+                if (result == null)
+                {
+                    throw new Exception("The list of all sensors not found.");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         // Get sensor by id.
         public async Task<Sensor> GetSensorAsync(string id)
         {
-            return await _dataService.GetSensorAsync(id);
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentNullException(nameof(id));
+                }
+                else
+                {
+                    var result = await _dataService.GetSensorAsync(id);
+
+                    if (result == null)
+                    {
+                        throw new Exception($"The sensor with the {id} Id is not found.");
+                    }
+                    else
+                    {
+                        return result;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         // Update sensor.
         public async Task UpdateSensorAsync(string id, Sensor newSensor)
         {
-            await _dataService.UpdateSensorAsync(id, newSensor);
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentNullException(nameof(id));
+                }
+                else if (newSensor == null)
+                {
+                    throw new ArgumentNullException(nameof(newSensor));
+                }
+                else
+                {
+                    await _dataService.UpdateSensorAsync(id, newSensor);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         // Delete sensor.
         public async Task DeleteSensorAsync(string id)
         {
-            await _dataService.DeleteAllSensorDataBySensorIdAsync(id);
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentNullException(nameof(id));
+                }
+                else
+                {
+                    await _dataService.DeleteAllSensorDataBySensorIdAsync(id);
 
-            await _dataService.DeleteSensorAsync(id);
+                    await _dataService.DeleteSensorAsync(id);
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
